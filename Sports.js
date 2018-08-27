5786582a147df540f1f79044f8d35ca8d09295ce
Bilalattari <attari1235@gmail.com>
2018-08-27 23:15:05 +0500

    sports page done

--- Sports.js
+++ Sports.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , ImageBackground } from 'react-native';
 import Header from './header'
-export default class App extends React.Component {
+export default class Sport extends React.Component {
   render() {
     return (
-      <View >
-      <Header heading = 'Sports' />
-      <Text> Sports </Text>
-      </View>
+        <ImageBackground source={require("./images/SportsBackground.png")}  style={{width: '100%', height: '100%'}}>
+        <View >
+        <Header heading = 'Sports' />
+        <View style = {{flexDirection : "row" , justifyContent : "space-around" , paddingTop : "6%"}}>
+  
+        <View style = {{flexDirection : "column" , height : 112 , width : "45%" ,  alignItems : "center" ,justifyContent : "center" ,
+           borderRadius : 5  , backgroundColor : "#EEF4F8"}}>
+        <Image  source = {require("./images/ProfileIcon.png")}  
+         style = {{height : 35 , width : 35 ,}}/>
+          <Text style = {{color : "#99BCCF" , paddingTop : 3}}> Profile </Text>
+        </View>
+  
+        <View style = {{flexDirection : "column" , height : 112 ,width : "45%" , borderColor : "#000" , justifyContent : "center" ,
+        borderRadius : 5 , backgroundColor : "#116389" , alignItems : "center" }}>
+        <Image  source = {require("./images/TimesheetsIcon.png")}  
+         style = {{height : 35 , width : 35 , }}/>
+        <Text style = {{color : "#fff" , paddingTop : 3}}> Timesheets </Text>
+        </View>
+  
+        </View>
+        
+        </View>
+        </ImageBackground >
     );
   }
 }
